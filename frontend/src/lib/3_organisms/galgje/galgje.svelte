5f7c7b5bae61a6309115deeff2d0be2ef457d1a1
Orim12 <miro.vaassen@gmail.com>
2024-09-11 15:09:01 +0200

    Refactor Galgje block styles and update project structure

--- frontend/src/lib/3_organisms/galgje/galgje.svelte
+++ frontend/src/lib/3_organisms/galgje/galgje.svelte
@@ -1,57 +1,28 @@
 <script lang="ts">
-  import Img from "@src/lib/1_atoms/img/img.svelte";
-  import type { GalgjeBlock } from "@src/payload-types";
+  import Container from "@src/lib/0_utils/container/container.svelte";
+import type { GalgjeBlock } from "@src/payload-types";
 
   export let content: GalgjeBlock;
-
-  function replaceBracketsWithSpan(text) {
-    const replacedText = text.replace(
-      /\{([^}]+)\}/g,
-      '<span class="underline">$1</span>'
-    );
-    return replacedText;
-  }
 </script>
 
-<section class="galgje">
-  {#if content?.title}
-    <div class="top-galgje">
-      <div class="lava-text-container">
-        <div class="lava-text">
-          <h3>
-            {@html replaceBracketsWithSpan(content?.title)}
-          </h3>
-        </div>
-      </div>
-      <div class="blocks">
-        <div class="back-end">
-          <div class="back-end-text">
-            <p>{content?.firstBlockText}</p>
-          </div>
-        </div>
-        <div class="front-end">
-          <div class="front-end-text">
-            <p>{content?.secondBlockText}</p>
-          </div>
-        </div>
-      </div>
+<section class="website">
+  <Container>
+  {#if content?.wordList}
+  <div class="galgje">
+    <div class="hangmanwood">
+      <div class="hangmanwood__1"></div>
+      <div class="hangmanwood__2"></div>
+      <div class="hangmanwood__3"></div>
+      <div class="hangmanwood__4"></div>
+      <div class="hangmanwood__5"></div>
+      <div class="hangmanwood__6"></div>
+      <img src="./icon/heuvel.svg" alt="heuvel" class="svg">
     </div>
-
-    <div class="bottom-galgje">
-      <div class="big-photo">
-        <Img content={content?.image} size="fullscreen" />
-      </div>
-    </div>
-    <div class="line-text">
-      {#if content?.wordsForBanner?.length > 0}
-        <p>
-          {#each content?.wordsForBanner as { word, id }, index}
-            {word}{#if index !== content?.wordsForBanner.length - 1}&nbsp;-&nbsp;{/if}
-          {/each}
-        </p>
-      {/if}
-    </div>
+  </div>
+  {:else}
+  <p>Er is geen woord gevonden</p>
   {/if}
+</Container>
 </section>
 
 <style lang="scss">
